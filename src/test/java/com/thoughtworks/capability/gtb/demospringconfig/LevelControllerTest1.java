ee8609954eea390ca6defb83ae645046e8ef9567
Xiran LYU <xiran.lyu@thoughtworks.com>
2020-10-27 21:45:56 +0800

    solution 3 added

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest1.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = "levelNumber=0")
+public class LevelControllerTest1 {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void should_return_basic_when_levelNumber_is_less_than_1() throws Exception {
+        mockMvc.perform(get("/level"))
+                .andExpect(content().string("basic"));
+    }
+}
---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest3.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = "levelNumber=2")
+public class LevelControllerTest3 {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void should_return_advanced_when_levelNumber_is_bigger_than_1() throws Exception {
+        mockMvc.perform(get("/level"))
+                .andExpect(content().string("advanced"));
+    }
+}
